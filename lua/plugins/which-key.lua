a3943930729e2052e713a2eee2b333e322082987
Oleksii Korzhuk <oleksii@koho.ca>
2024-04-05 10:36:46 -0400

    Add neo-vim and fine-cmdline

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,21 +6,11 @@ return {
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup({
 				triggers_blacklist = {
-					-- list of mode / prefixes that should never be hooked by WhichKey
-					-- this is mostly relevant for keymaps that start with a native binding
 					i = { "<leader>", "j", "k" },
 					v = { "j", "k" },
 				},
+				ignore_missing = true,
 			})
-
-			-- Document existing key chains
-			-- require("which-key").register({
-			--	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			--	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			--	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			--	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			--	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			--})
 		end,
 	},
 }
