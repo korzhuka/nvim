e55e99c3d6e3bfbfe64b23adf5e56610b83e2c09
Oleksii Korzhuk <mail@korzhuk.io>
2024-04-16 22:33:31 -0400

    Allows C-n and C-p behaive as Up and Down

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -33,8 +33,8 @@ return {
 				},
 
 				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 
