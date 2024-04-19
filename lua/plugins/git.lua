53dd0280f8df192928ee481e015661cb3959bb73
Oleksii Korzhuk <mail@korzhuk.io>
2024-04-19 10:34:52 -0400

    Configure hunks operations in Gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,6 +14,40 @@ return {
 					changedelete = { text = "~" },
 				},
 				current_line_blame = true,
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					map("n", "]h", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end, { desc = "Next [H]unk " })
+
+					map("n", "[h", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end, { desc = "Prev [H]unk " })
+
+					map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage [H]unk " })
+					map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset [H]unk " })
+					map("v", "<leader>hs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "Stage selected [H]unk " })
+					map("v", "<leader>hr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "Reset selected [H]unk " })
+				end,
 			})
 		end,
 	},
