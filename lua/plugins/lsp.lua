92a22ecc8785528790eb800123645814a8984655
Oleksii Korzhuk <mail@korzhuk.io>
2024-05-23 08:17:32 -0400

    Add ensure_installed for Mason and disable diagnostics keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,6 +56,17 @@ return {
 		lazy = false,
 		opts = {
 			auto_install = true,
+			enshure_installed = {
+				"json-lsp",
+				"helm-ls",
+				"lua-language-server",
+				"goimports-reviser",
+				"goimports",
+				"prettier",
+				"stylua",
+				"terraform-ls",
+				"yaml-language-server",
+			},
 		},
 	},
 	{
@@ -103,8 +114,6 @@ return {
 				map("<C-p>", vim.lsp.buf.signature_help, "Signature help", "i")
 			end
 
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
