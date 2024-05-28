a8787002681ea6279242471c73af7048863d5a0d
Oleksii Korzhuk <mail@korzhuk.io>
2024-05-28 12:28:06 -0400

    Add tree-sitter text-object

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -2,14 +2,36 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "go", "lua", "yaml" },
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = { query = "@function.outer", desc = "Navigation: Next function" },
+							["]M"] = { query = "@function.inner", desc = "Navigation: Next function in" },
+							["]]"] = { query = "@class.outer", desc = "Navigation: Next class start" },
+							["]z"] = { query = "@fold", query_group = "folds", desc = "Navigation: Next fold" },
+							["]p"] = { query = "@parameter.inner", desc = "Navigation: Next parameter" },
+						},
+						goto_previous_start = {
+							["[m"] = { query = "@function.outer", desc = "Navigation: Prev function" },
+							["[["] = { query = "@class.outer", desc = "Navigation: Prev class start" },
+							["[z"] = { query = "@fold", query_group = "folds", desc = "Navigation: Prev fold" },
+							["[p"] = { query = "@parameter.inner", desc = "Navigation: Prev parameter" },
+						},
+					},
+				},
 			})
 		end,
-		vasia = true,
 	},
 }
