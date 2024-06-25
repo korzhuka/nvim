26c70bfece53eee2550e596afd6ab22fb50910fa
Oleksii Korzhuk <oleksii@koho.ca>
2024-06-25 14:33:38 -0400

    LSP ENOENT error fix

--- lua/miscs.lua
+++ lua/miscs.lua
@@ -6,3 +6,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- General/Global LSP Configuration
+local api = vim.api
+local lsp = vim.lsp
+
+local make_client_capabilities = lsp.protocol.make_client_capabilities
+function lsp.protocol.make_client_capabilities()
+	local caps = make_client_capabilities()
+	if not (caps.workspace or {}).didChangeWatchedFiles then
+		vim.notify("lsp capability didChangeWatchedFiles is already disabled", vim.log.levels.WARN)
+	else
+		caps.workspace.didChangeWatchedFiles = nil
+	end
+
+	return caps
+end
