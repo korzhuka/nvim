5abe0e0b5c9ee01229c7da22fbf31f4e620d32ed
Oleksii Korzhuk <oleksii@koho.ca>
2024-08-07 21:03:51 -0400

    User CTRL+e to close complition popup

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -35,6 +35,7 @@ return {
 				mapping = cmp.mapping({
 					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+					["<C-e>"] = cmp.mapping.close(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 
