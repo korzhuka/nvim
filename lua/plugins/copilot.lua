e364940be04f28586df452bb26e88ca8692033b9
Oleksii Korzhuk <oleksii@koho.ca>
2024-11-14 14:28:27 -0500

    Update copilot bindings

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,14 +2,16 @@ return {
 	{
 		"https://github.com/github/copilot.vim",
 		config = function()
-			local set = function(key, cmd, desc)
-				vim.keymap.set("i", key, "<Plug>(" .. cmd .. ")", { desc = "Copilot: " .. desc })
+			local set = function(key, cmd, _)
+				vim.keymap.set("i", key, "<Plug>(" .. cmd .. ")")
 			end
 
-			set("<C-]>", "copilot-dismiss", "Dismiss the current suggestion.")
+			set("<D-d>", "copilot-dismiss", "Dismiss the current suggestion.")
+			set("<D-s>", "copilot-suggest", "Explicitly request a suggestion, even if Copilot is disabled.")
+
 			set("<D-]>", "copilot-next", "Cycle to the next suggestion, if one is available.")
 			set("<D-[>", "copilot-previous", "Cycle to the previous suggestion.")
-			set("<D-i>", "copilot-suggest", "Explicitly request a suggestion, even if Copilot is disabled.")
+
 			set("<D-Right>", "copilot-accept-word", "Accept the next word of the current suggestion.")
 			set("<D-C-Right>", "copilot-accept-line", "Accept the next line of the current suggestion.")
 		end,
