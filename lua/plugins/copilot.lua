a726d3516686c92c7a3c0f12b31c78d90d246c9f
Oleksii Korzhuk <oleksii@koho.ca>
2025-02-25 17:13:34 -0500

    Add copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,14 +6,31 @@ return {
 				vim.keymap.set("i", key, "<Plug>(" .. cmd .. ")")
 			end
 
-			set("<D-d>", "copilot-dismiss", "Dismiss the current suggestion.")
-			set("<D-s>", "copilot-suggest", "Explicitly request a suggestion, even if Copilot is disabled.")
+			set("<M-d>", "copilot-dismiss", "Dismiss the current suggestion.")
+			set("<M-s>", "copilot-suggest", "Explicitly request a suggestion, even if Copilot is disabled.")
 
-			set("<D-]>", "copilot-next", "Cycle to the next suggestion, if one is available.")
-			set("<D-[>", "copilot-previous", "Cycle to the previous suggestion.")
+			set("<M-]>", "copilot-next", "Cycle to the next suggestion, if one is available.")
+			set("<M-[>", "copilot-previous", "Cycle to the previous suggestion.")
 
-			set("<D-Right>", "copilot-accept-word", "Accept the next word of the current suggestion.")
-			set("<D-C-Right>", "copilot-accept-line", "Accept the next line of the current suggestion.")
+			set("<M-aw>", "copilot-accept-word", "Accept the next word of the current suggestion.")
+			set("<M-l>", "copilot-accept-line", "Accept the next line of the current suggestion.")
 		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken",
+		opts = {
+			chat_window = {
+				position = "right",
+				width = 50,
+			},
+			chat_input = {
+				prompt = "Copilot Chat:",
+			},
+		},
 	},
 }
