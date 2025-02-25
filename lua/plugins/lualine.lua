6edfab42d5921cf6abad7927641c9e3ed91a523e
Oleksii Korzhuk <oleksii@koho.ca>
2025-02-25 17:15:06 -0500

    Add LSP icon to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,27 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
+	{
+		"j-hui/fidget.nvim",
+		opts = {},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"yavorski/lualine-lsp-client-name.nvim",
+		},
+		config = function()
+			require("lualine").setup({
 				theme = "catppuccin-mocha",
 				sections = {
-					lualine_a = {
-						file = 1,
-					},
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename" },
+					lualine_x = { "lsp_client_name", "encoding", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
 				},
-			},
-		})
-	end,
+				inactive_sections = {},
+			})
+		end,
+	},
 }
