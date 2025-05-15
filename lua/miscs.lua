6486eb9a7d5532b3f148d3a454773a1646fc36cf
Oleksii Korzhuk <oleksii@koho.ca>
2025-05-15 16:15:45 -0400

    Remove 'didChangedWatchedFiles' capability initialization

--- lua/miscs.lua
+++ lua/miscs.lua
@@ -7,18 +7,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- General/Global LSP Configuration
-local api = vim.api
-local lsp = vim.lsp
-
-local make_client_capabilities = lsp.protocol.make_client_capabilities
-function lsp.protocol.make_client_capabilities()
-	local caps = make_client_capabilities()
-	if not (caps.workspace or {}).didChangeWatchedFiles then
-		vim.notify("lsp capability didChangeWatchedFiles is already disabled", vim.log.levels.WARN)
-	else
-		caps.workspace.didChangeWatchedFiles = nil
-	end
-
-	return caps
-end
+-- -- General/Global LSP Configuration
+-- local api = vim.api
+-- local lsp = vim.lsp
+--
+-- local make_client_capabilities = lsp.protocol.make_client_capabilities
+-- function lsp.protocol.make_client_capabilities()
+-- 	local caps = make_client_capabilities()
+-- 	if not (caps.workspace or {}).didChangeWatchedFiles then
+-- 		vim.notify("lsp capability didChangeWatchedFiles is already disabled", vim.log.levels.WARN)
+-- 	else
+-- 		caps.workspace.didChangeWatchedFiles = nil
+-- 	end
+--
+-- 	return caps
+-- end
